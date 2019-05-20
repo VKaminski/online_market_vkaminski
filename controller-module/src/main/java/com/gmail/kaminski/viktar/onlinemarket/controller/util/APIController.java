7afdc2bdc160dd4eadc0a5cd33045a27f884b95a
VKaminski <viktar.kaminski@gmail.com>
2019-05-20 07:26:35 +0300

    Synchronized commit

--- controller-module/src/main/java/com/gmail/kaminski/viktar/onlinemarket/controller/util/APIController.java
+++ controller-module/src/main/java/com/gmail/kaminski/viktar/onlinemarket/controller/util/APIController.java
@@ -1,12 +1,15 @@
 package com.gmail.kaminski.viktar.onlinemarket.controller.util;
 
 import com.gmail.kaminski.viktar.onlinemarket.controller.CustomerController;
+import com.gmail.kaminski.viktar.onlinemarket.service.ArticleService;
+import com.gmail.kaminski.viktar.onlinemarket.service.UserService;
 import com.gmail.kaminski.viktar.onlinemarket.service.model.ArticleDTO;
-import com.gmail.kaminski.viktar.onlinemarket.service.model.UserDTO;
+import com.gmail.kaminski.viktar.onlinemarket.service.model.NewUserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,26 +26,40 @@ import java.util.List;
 public class APIController {
     private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
     private static final Marker custom = MarkerFactory.getMarker("custom");
+    private UserService userService;
+    private ArticleService articleService;
+
+    public APIController(UserService userService, ArticleService articleService) {
+        this.userService = userService;
+        this.articleService = articleService;
+    }
 
     @PostMapping("/users")
-    public UserDTO addUser(
-            @RequestBody UserDTO userDTO) {
-
+    public ResponseEntity<String> addUser(
+            @RequestBody NewUserDTO newUserDTO) {
+        userService.add(newUserDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/articles")
-    public List<ArticleDTO> getArticles() {
-
+    public ResponseEntity<List<ArticleDTO>> getArticles(
+            @PathVariable Long firstElement,
+            @PathVariable Integer amountElement
+    ) {
+        List<ArticleDTO> articles = articleService.getArticles(firstElement, amountElement);
+        return new ResponseEntity<>(articles, HttpStatus.OK);
     }
 
     @GetMapping("/articles/{id}")
-    public ArticleDTO getArticle() {
+    public ArticleDTO getArticle(
+            @PathVariable Long id
+    ) {
 
     }
 
     @PostMapping("/articles")
     public List<ArticleDTO> addArticles(
-            @RequestBody     ) {
+            @RequestBody) {
 
     }
 
